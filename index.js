a3861ce7203655072cb8462922b8743319ba9c85
ararara127 <azzahraindah127@gmail.com>
2025-12-07 22:07:44 +0700

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Render PaaS!');
+  res.send('Hello from Railway PaaS!');
 });
 
 const port = process.env.PORT || 3000;
